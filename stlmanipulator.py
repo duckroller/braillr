93c0e03f825c519301f3f11a9dd9a9392c9aa631
duckroller <duckroller@gmail.com>
2017-02-18 22:22:26 -0500

    sti rotator added, webAccess cleaned
    added smart wait to webAccess, made a function so it's not some nasty thing

---
+++ stlmanipulator.py
@@ -0,0 +1,13 @@
+import numpy
+import stl
+from stl import mesh
+import math
+
+def rotate(filename):
+    """rotates an stl mesh 90 degrees about the x axis"""
+
+    main_body = mesh.Mesh.from_file(filename)
+
+    main_body.rotate([0.5, 0.0, 0.0], math.radians(90))
+
+    main_body.save('rotated_' + filename)
--- webAccess.py
+++ webAccess.py
@@ -1,31 +1,44 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
-import requests, sys, webbrowser, bs4, time
-
-#content_type = requests.head('http://v01pe.github.io/6e7f89b1-94ad-403e-815d-f3b93fd0ec8b').headers['content-type']
-#print(content_type)
+import requests, sys, webbrowser, time
 
 profile = FirefoxProfile()
-profile.set_preference("browser.helperApps.neverAsk.saveToDisk", 'application/sla')
+profile.set_preference("browser.helperApps.neverAsk.saveToDisk", 'application/vnd.ms-pki.stl')
 driver = webdriver.Firefox()#firefox_profile=profile)
 driver.get('http://v01pe.github.io/Text2Braille3d/')
 
-text_form = driver.find_element_by_css_selector('.parameterstable > tr:nth-child(1) > td:nth-child(2) > textarea:nth-child(1)')
+def generate_stl(input_text):
+    # input text to be printed
+    text_form = driver.find_element_by_css_selector('.parameterstable > tr:nth-child(1) > td:nth-child(2) > textarea:nth-child(1)')
+    text_form.clear()
+    text_form.send_keys("python")
 
-text_form.clear()
-text_form.send_keys("python")
+    # open advanced options
+    advanced_tickbox = driver.find_element_by_css_selector('#more')
+    advanced_tickbox.click()
 
-submit_button = driver.find_element_by_css_selector('.parametersdiv > button:nth-child(5)')
+    # disable support circles
+    support_option = driver.find_element_by_css_selector('.parameterstable > tr:nth-child(11) > td:nth-child(2) > input:nth-child(1)')
+    support_option.click()
 
-submit_button.click()
+    # submit text for rendering
+    submit_button = driver.find_element_by_css_selector('.parametersdiv > button:nth-child(5)')
+    submit_button.click()
 
-time.sleep(20)
+    # generate stl file
+    wait = WebDriverWait(driver, 30)
+    stl_generate_button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR,'.statusdiv > div:nth-child(2) > button:nth-child(4)')))
+    #stl_generate_button = driver.find_element_by_css_selector('.statusdiv > div:nth-child(2) > button:nth-child(4)')
 
-stl_generate_button = driver.find_element_by_css_selector('.statusdiv > div:nth-child(2) > button:nth-child(4)')
+    stl_generate_button.click()
 
-stl_generate_button.click()
+    # download stl file
+    stl_downlowd_button = driver.find_element_by_css_selector('.statusdiv > div:nth-child(2) > a:nth-child(5)')
+    stl_downlowd_button.click()
 
-stl_downlowd_button = driver.find_element_by_css_selector('.statusdiv > div:nth-child(2) > a:nth-child(5)')
-
-stl_downlowd_button.click()
+if __name__ == '__main__':
+    generate_stl('python')
